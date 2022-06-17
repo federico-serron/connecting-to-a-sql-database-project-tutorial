1469e90e470f5953c90d8fb56f9024169e479487
Federico Serron Umpierrez <f.serron.umpierrez@gmail.com>
2022-06-17 07:53:40 +0000

    Working on connection

--- src/exercise.py
+++ src/exercise.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
+import connection
 import psycopg2
 import pandas as pd
 
-load_dotenv()
-print(os.getenv('DB_USER'))
-conn = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'))
-
+conn = connection.connect()
 cursor = conn.cursor()
 # cursor.execute('SELECT * FROM Movies')
 
